b20865b073198a5f9fba286e86cbc6a0d5fe0156
dmakokha <david@guruit.co.ke>
2015-10-27 13:49:43 +0300

    My Django Girls app, first commit

---
+++ django-admin-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\DAVID\AppData\Local\Programs\Python\Python35-32\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Django==1.8.5','console_scripts','django-admin'
+__requires__ = 'Django==1.8.5'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Django==1.8.5', 'console_scripts', 'django-admin')()
+    )
